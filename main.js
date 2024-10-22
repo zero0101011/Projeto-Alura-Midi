66ca79eee4cea189e907f2b13d67eff52866d1df
Samuel Carlos <102079472+zero0101011@users.noreply.github.com>
2024-10-22 18:29:40 -0300

    update JavaScript

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
-function tocasompom () {
-   document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+   document.querySelector(idElementoAudio).play();
 }
 
-  
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas[0].onclick = tocaSom;
+
+let contador = 0;
+
+while(contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador];
+
+    const instrumento = tecla.classList[1];
+
+    const idAudio = `#som_${instrumento}`
+
+   tecla.onclick = function () {
+      tocaSom(idAudio)
+   }
+
+
+   contador = contador + 1
+
+}
