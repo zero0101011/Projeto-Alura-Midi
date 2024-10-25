170627eca80cf96d3d262e6dcca6c78c2ae3a750
Samuel Carlos <102079472+zero0101011@users.noreply.github.com>
2024-10-25 16:21:11 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
-function tocaSom (idElementoAudio) {
-   document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+   const elemento = document.querySelector(seletorAudio);
+
+
+  if (elemento != 'null' && elemento.localName === 'audio') {
+
+   elemento.play();
+
+  } else {
+    
+   console.log('elemento não encontrado ou seletor inválido')
+    
+
+  }
+
+
+   
+
+
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
